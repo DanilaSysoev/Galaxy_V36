2ffc288e243fc77b22aeae3bf63bdf690aa3fc9f
DanilaMSU <78799957+DanilaSysoev@users.noreply.github.com>
2021-08-31 13:47:28 +0300

    add Vector3_ZeroVector_ComponentsEqualsZero

---
+++ Galaxy_V36/Galaxy_V36_Tests/Vector3Tests.cs
@@ -0,0 +1,20 @@
+﻿using Galaxy_V36.Service;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Galaxy_V36_Tests
+{
+    [TestFixture]
+    class Vector3Tests
+    {
+        [TestCase]
+        public void Vector3_ZeroVector_ComponentsEqualsZero()
+        {
+            Assert.AreEqual(0, Vector3.Zero.X);
+            Assert.AreEqual(0, Vector3.Zero.Y);
+            Assert.AreEqual(0, Vector3.Zero.Z);
+        }
+    }
+}
