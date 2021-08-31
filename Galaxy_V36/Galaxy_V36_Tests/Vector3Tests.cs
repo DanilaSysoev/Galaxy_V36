1e2659c35f373a50119b43af85f7b020b058fe49
DanilaMSU <78799957+DanilaSysoev@users.noreply.github.com>
2021-08-31 14:39:36 +0300

    add Equals_VectorsWithNonStrongDifferentThreeComponents_ReturnFalse

--- Galaxy_V36/Galaxy_V36_Tests/Vector3Tests.cs
+++ Galaxy_V36/Galaxy_V36_Tests/Vector3Tests.cs
@@ -117,5 +117,16 @@ namespace Galaxy_V36_Tests
             Vector3 vector2 = new Vector3(1, 2, 3 + 2 * Configuration.FloatEpsilon);
             Assert.IsFalse(vector1.Equals(vector2));
         }
+        [TestCase]
+        public void Equals_VectorsWithNonStrongDifferentThreeComponents_ReturnFalse()
+        {
+            Vector3 vector1 = new Vector3(1, 2, 3);
+            Vector3 vector2 =
+                new Vector3(
+                    1 + 2 * Configuration.FloatEpsilon,
+                    2 - 2 * Configuration.FloatEpsilon,
+                    3 + 2 * Configuration.FloatEpsilon);
+            Assert.IsFalse(vector1.Equals(vector2));
+        }
     }
 }
