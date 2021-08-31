2e33edaea884fa2112a9a9421f4a77ff32728f84
DanilaMSU <78799957+DanilaSysoev@users.noreply.github.com>
2021-08-31 13:21:07 +0300

    add SpaceShip_CreationShip_PositionPropertyReturnsZeroVector

---
+++ Galaxy_V36/Galaxy_V36_Tests/SpaceShipTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Galaxy_V36_Tests
+{
+    [TestFixture]
+    class SpaceShipTests
+    {
+        [TestCase]
+        public void SpaceShip_CreationShip_PositionPropertyReturnsZeroVector()
+        {
+            SpaceShip spaceShip = GameEntityBuilder.CreateSpaceShip();
+            Assert.AreEqual(Vector3.Zero, spaceShip.Position);
+        }
+    }
+}
