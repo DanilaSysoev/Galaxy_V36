131349f2f890f6653a7e36635e1aab0b8c813a2c
DanilaMSU <78799957+DanilaSysoev@users.noreply.github.com>
2021-09-17 17:53:35 +0300

    add simple empty ui

---
+++ Galaxy_V36/Galaxy_V36_ConsoleUI/ConsoleUserInterface.cs
@@ -0,0 +1,34 @@
+﻿using Galaxy_V36.UI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Galaxy_V36_ConsoleUI
+{
+    public class ConsoleUserInterface : IUserInterface
+    {
+        public void InitializeEnd()
+        {
+        }
+
+        public void InitializeStart()
+        {
+        }
+
+        public void LoadEnd()
+        {
+        }
+
+        public void LoadStart()
+        {
+        }
+
+        public void PrepareEnd()
+        {
+        }
+
+        public void PrepareStart()
+        {
+        }
+    }
+}
---
+++ Galaxy_V36/Galaxy_V36_ConsoleUI/Program.cs
@@ -0,0 +1,14 @@
+﻿using Galaxy_V36.Engine;
+using System;
+
+namespace Galaxy_V36_ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game(new ConsoleUserInterface());
+            game.Run();
+        }
+    }
+}
