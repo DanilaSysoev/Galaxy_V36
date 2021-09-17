c60d57a65235f2f103439073c7e0664b8762acc9
DanilaMSU <78799957+DanilaSysoev@users.noreply.github.com>
2021-09-17 17:31:57 +0300

    create base carcas

---
+++ Galaxy_V36/Galaxy_V36/Engine/Game.cs
@@ -0,0 +1,60 @@
+﻿using Galaxy_V36.UI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Galaxy_V36.Engine
+{
+    public class Game
+    {
+        public Game(IUserInterface userInterface)
+        {
+            this.userInterface = userInterface;
+        }
+
+        public void Run()
+        {            
+            Prepare();
+            Load();
+            Initialize();
+            while (Tik()) ;
+        }
+
+        private void Prepare()
+        {
+            userInterface.PrepareStart();
+            PreparationProcedure();
+            userInterface.PrepareEnd();
+        }
+        private void PreparationProcedure()
+        {
+        }
+
+        private void Load()
+        {
+            userInterface.LoadStart();
+            LoadingProcedure();
+            userInterface.LoadEnd();
+        }
+        private void LoadingProcedure()
+        {
+        }
+
+        private void Initialize()
+        {
+            userInterface.InitializeStart();
+            InitializationProcedure();
+            userInterface.InitializeEnd();
+        }
+        private void InitializationProcedure()
+        {
+        }
+
+        private bool Tik()
+        {
+            return false;
+        }
+
+        private IUserInterface userInterface;
+    }
+}
---
+++ Galaxy_V36/Galaxy_V36/UI/IUserInterface.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Galaxy_V36.UI
+{
+    public interface IUserInterface
+    {
+        void PrepareStart();
+        void PrepareEnd();
+
+        void LoadStart();
+        void LoadEnd();
+
+        void InitializeStart();
+        void InitializeEnd();
+    }
+}
