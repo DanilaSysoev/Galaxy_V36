280720934623b9aeb470fca30fec11b43c809023
DanilaMSU <78799957+DanilaSysoev@users.noreply.github.com>
2021-09-17 15:15:57 +0300

    add Build_BuildFromstringWithNameEnergyAndUniversalRecords_BuildingOk

---
+++ Galaxy_V36/Galaxy_V36_Tests/BlueprintTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Galaxy_V36_Tests
+{
+    [TestFixture]
+    class BlueprintTests
+    {
+        [TestCase]
+        public void Build_BuildFromstringWithNameEnergyAndUniversalRecords_BuildingOk()
+        {
+            string bpDescr = 
+                "Name: Default\n" +
+                "Type: Ship\n" +
+                "Universal: 2\n" +
+                "Energy: 5";
+            Blueprint blueprint = Blueprint.Build(bpDescr);
+            Assert.AreEqual("Default", blueprint.Name);
+            Assert.AreEqual(BlueprintType.Ship, blueprint.Type);
+            Assert.AreEqual(2, blueprint.UniversalInterfacesCount);
+            Assert.AreEqual(5, blueprint.EnergyInterfacesCount);
+
+        }
+    }
+}
