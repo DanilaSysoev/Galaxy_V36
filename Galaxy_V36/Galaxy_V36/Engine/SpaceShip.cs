e2d054df0c780b8ab7d22dea2b534d292539035d
DanilaMSU <78799957+DanilaSysoev@users.noreply.github.com>
2021-09-20 20:46:14 +0300

    add SpaceShip with speed

---
+++ Galaxy_V36/Galaxy_V36/Engine/SpaceShip.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Galaxy_V36.Engine
+{
+    public class SpaceShip
+    {
+        public float Speed { get; private set; }
+
+        public SpaceShip(float speed)
+        {
+            Speed = speed;
+        }
+    }
+}
