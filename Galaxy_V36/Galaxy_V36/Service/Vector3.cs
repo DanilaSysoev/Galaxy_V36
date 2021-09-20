941d0b6d01776518bd976ce02a72c27fdc8a4332
DanilaMSU <78799957+DanilaSysoev@users.noreply.github.com>
2021-09-20 16:59:25 +0300

    add Vector3

---
+++ Galaxy_V36/Galaxy_V36/Service/Vector3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Galaxy_V36.Service
+{
+    public class Vector3
+    {
+        public float X { get; private set; }
+        public float Y { get; private set; }
+        public float Z { get; private set; }
+
+        public Vector3()
+        {
+            X = Y = Z = 0;
+        }
+        public Vector3(float x, float y, float z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+    }
+}
