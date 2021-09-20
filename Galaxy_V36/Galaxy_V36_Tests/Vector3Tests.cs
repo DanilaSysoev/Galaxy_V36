62027f77980a358252aff7b1399ab23d478f9567
DanilaMSU <78799957+DanilaSysoev@users.noreply.github.com>
2021-09-20 17:02:18 +0300

    add Vector3_CreationWithoutParams_CoordinatesEqualsZero

---
+++ Galaxy_V36/Galaxy_V36_Tests/Vector3Tests.cs
@@ -0,0 +1,21 @@
+﻿using Galaxy_V36.Service;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Galaxy_V36_Tests
+{
+    [TestFixture]
+    class Vector3Tests
+    {
+        [TestCase]
+        public void Vector3_CreationWithoutParams_CoordinatesEqualsZero()
+        {
+            Vector3 vector3 = new Vector3();
+            Assert.AreEqual(0, vector3.X);
+            Assert.AreEqual(0, vector3.Y);
+            Assert.AreEqual(0, vector3.Z);
+        }
+    }
+}
