93fca587b2cef4710856bccfd22d2b45ba5f16cb
DanilaMSU <78799957+DanilaSysoev@users.noreply.github.com>
2021-09-20 21:00:06 +0300

    add Player_CreateWithShip_ShipPropertyIsOk

--- Galaxy_V36/Galaxy_V36_Tests/PlayerTests.cs
+++ Galaxy_V36/Galaxy_V36_Tests/PlayerTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using Galaxy_V36.Engine;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,5 +9,12 @@ namespace Galaxy_V36_Tests
     [TestFixture]
     class PlayerTests
     {
+        [TestCase]
+        public void Player_CreateWithShip_ShipPropertyIsOk()
+        {
+            SpaceShip ship = new SpaceShip(10);
+            Player player = new Player(ship);
+            Assert.AreEqual(ship, player.Ship);
+        }
     }
 }
