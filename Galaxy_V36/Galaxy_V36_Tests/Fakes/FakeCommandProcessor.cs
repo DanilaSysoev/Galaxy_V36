13b6a66a78c71711af112c06145ba08c443cd7d6
DanilaMSU <78799957+DanilaSysoev@users.noreply.github.com>
2021-09-20 21:38:10 +0300

    add FakeCommandProcessor and Create_Creation_CommandProcessedIsFalse

---
+++ Galaxy_V36/Galaxy_V36_Tests/Fakes/FakeCommandProcessor.cs
@@ -0,0 +1,25 @@
+﻿using Galaxy_V36.Engine;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Galaxy_V36_Tests.Fakes
+{
+    public class FakeCommandProcessor : ICommandProcessor
+    {
+        public bool CommandProcessed { get; private set; }
+        public string LastProcessedCommand { get; private set; }
+
+        public FakeCommandProcessor()
+        {
+            CommandProcessed = false;
+            LastProcessedCommand = null;
+        }
+
+        public void ProcessCommand(string command)
+        {
+            CommandProcessed = true;
+            LastProcessedCommand = command;
+        }
+    }
+}
---
+++ Galaxy_V36/Galaxy_V36_Tests/Fakes/FakeCommandProcessorTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Galaxy_V36_Tests.Fakes
+{
+    [TestFixture]
+    class FakeCommandProcessorTests
+    {
+        [TestCase]
+        public void Create_Creation_CommandProcessedIsFalse()
+        {
+            FakeCommandProcessor fcp = new FakeCommandProcessor();
+            Assert.IsFalse(fcp.CommandProcessed);
+        }
+    }
+}
