ee472e660b5b7fcd78a67aa92b3095e4a89ebf2c
DanilaMSU <78799957+DanilaSysoev@users.noreply.github.com>
2021-09-20 20:55:51 +0300

    add SpaceShip_CreationWithSpeed_SpeedPropertySetOk

---
+++ Galaxy_V36/Galaxy_V36_Tests/PlayerTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Galaxy_V36_Tests
+{
+    [TestFixture]
+    class PlayerTests
+    {
+    }
+}
---
+++ Galaxy_V36/Galaxy_V36_Tests/SpaceShipTest.cs
@@ -0,0 +1,19 @@
+﻿using Galaxy_V36.Engine;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Galaxy_V36_Tests
+{
+    [TestFixture]
+    class SpaceShipTest
+    {
+        [TestCase]
+        public void SpaceShip_CreationWithSpeed_SpeedPropertySetOk()
+        {
+            SpaceShip ship = new SpaceShip(10.1f);
+            Assert.AreEqual(10.1f, ship.Speed);
+        }
+    }
+}
