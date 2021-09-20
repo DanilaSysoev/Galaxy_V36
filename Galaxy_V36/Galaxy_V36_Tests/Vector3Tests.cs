0329aaedd853d3fc6e7f61bb0586bbf0d270c182
DanilaMSU <78799957+DanilaSysoev@users.noreply.github.com>
2021-09-20 17:29:34 +0300

    Equals_IntComponentsEquals_Equal

--- Galaxy_V36/Galaxy_V36_Tests/Vector3Tests.cs
+++ Galaxy_V36/Galaxy_V36_Tests/Vector3Tests.cs
@@ -69,5 +69,13 @@ namespace Galaxy_V36_Tests
             Assert.IsFalse(v1.Equals(v2));
             Assert.IsFalse(v2.Equals(v1));
         }
+        [TestCase]
+        public void Equals_IntComponentsEquals_Equal()
+        {
+            Vector3 v1 = new Vector3(1, 2, 3);
+            Vector3 v2 = new Vector3(1, 2, 3);
+            Assert.IsTrue(v1.Equals(v2));
+            Assert.IsTrue(v2.Equals(v1));
+        }
     }
 }
